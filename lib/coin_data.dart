15f5b42f939cc7ae4198dc0bb3b9e3e6688ae3b5
nuaacux <64741800+doro715@users.noreply.github.com>
2022-08-19 15:43:09 +0800

    Add files via upload

---
+++ lib/coin_data.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const List<String> currenciesList = [
+  'AUD',
+  'BRL',
+  'CAD',
+  'CNY',
+  'EUR',
+  'GBP',
+  'HKD',
+  'IDR',
+  'ILS',
+  'INR',
+  'JPY',
+  'MXN',
+  'NOK',
+  'NZD',
+  'PLN',
+  'RON',
+  'RUB',
+  'SEK',
+  'SGD',
+  'USD',
+  'ZAR'
+];
+
+const List<String> cryptoList = [
+  'BTC',
+  'ETH',
+  'LTC',
+];
+
+const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = '45A54BF5-7093-4634-A179-FF60677AFC48';
+
+class CoinData {
+  Future getCoinData(String selectedCurrency) async {
+    String requestURL = '$coinAPIURL/BTC$selectedCurrency';
+    http.Response response = await http.get(
+        Uri.parse('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=45A54BF5-7093-4634-A179-FF60677AFC48'));
+    if (response.statusCode == 200) {
+      var decodedData = jsonDecode(response.body);
+      var lastPrice = decodedData['rate'];
+      return lastPrice;
+    } else {
+      print(response.statusCode);
+      throw 'Problem with the get request';
+    }
+  }
+}
